961094f4ac25c7483453d4bcde59e0202008fa7c
tbarriofer <tbarriofer@gmail.com>
2026-04-07 13:17:43 +0100

    implement Book entity and repository with initial data fetch

--- libreria/src/main/java/com/library/libreria/LibreriaApplication.java
+++ libreria/src/main/java/com/library/libreria/LibreriaApplication.java
@@ -1,23 +1,28 @@
 package com.library.libreria;
 
+import com.library.libreria.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.library.libreria.repository.BookRepository;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LibreriaApplication implements CommandLineRunner {
 
-	@Autowired
-	private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibreriaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Aplicación iniciada correctamente.");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Aplicación iniciada correctamente.");
+        List<Book> books = bookRepository.findAll();
+        books.forEach(System.out::println);
+    }
 }
--- libreria/src/main/java/com/library/libreria/entity/Book.java
+++ libreria/src/main/java/com/library/libreria/entity/Book.java
@@ -1,11 +1,9 @@
 package com.library.libreria.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,4 +11,14 @@ public class Book {
     private String title;
     private String author;
     private String status;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
